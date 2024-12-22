265e95478d87c75433ffd8118d83a7bcd50fdd1d
enaaacode <152639159+enaaacode@users.noreply.github.com>
2024-12-22 22:52:25 +0100

    Calculation optimized

--- 3_Largest-Prime-Factor.py
+++ 3_Largest-Prime-Factor.py
@@ -1,45 +1,57 @@
 # Problem 3: Largest Prime Factor
 # we are looking for the biggest prime factor in 600851475143
 
-n = 1346
-#600851
+number = 600851475143
 
-# create a list with prime numbers
-# there is a algorithm to determine the prime numbers called "Sieve of the Eratosthenes" (Sieb des Eratosthenes)
+# - - - create a list with prime numbers - - -
+n = 10000 
+all_n = list(range(2,int(n+1)))
+prime_numbers = []
 
-numbers = list(range(2,n+1)) # start at 2 because 1 is not a prime number
-prime_numbers_lst = numbers 
+while len(prime_numbers) < 1000:
 
-list_counter = 0
-for i in range(1, len(numbers)):
-    if i < len(numbers):
-        if numbers[i] % numbers[list_counter] == 0:
-            prime_numbers_lst.remove(numbers[i])
-            list_counter += 1
+    l = [] # put the first entry from all_n in l 
+    l.append(all_n[0]) # this is the prime number we want to create multiples of
+    prime_numbers.append(all_n[0])
+    del all_n[0] # delete it from all_n
+    
+    for multiplicator in range(2, int(n/2)):
+        result = multiplicator * l[0]
+        if result > n:
+            break
+        else:
+            l.append(result)
 
-# this takes way to long T.T
+    # remove the entries in l from all_n
+    for entry in l:
+        if entry in all_n:
+            all_n.remove(entry)
 
-# find the prime factors of n
+    l.clear() # clear l to start over again with the next all_n[0]
 
-prime_factors_of_n = []
+print(prime_numbers)
+
+# - - - find the prime factors of number - - -
+
+prime_factors = []
 still_calculating = True
 
 counter = 0
-divided_n = n
 
 while still_calculating:
 
-    if counter < len(prime_numbers_lst):
+    if counter < len(prime_numbers):
 
-        if divided_n % prime_numbers_lst[counter] == 0:
-            divided_n = divided_n / prime_numbers_lst[counter] 
-            prime_factors_of_n.append(prime_numbers_lst[counter]) 
+        if number % prime_numbers[counter] == 0:
+            number = number / prime_numbers[counter]
+            if number not in prime_factors: 
+                prime_factors.append(prime_numbers[counter]) 
 
         else:
             counter += 1
         
-    if divided_n == 1:
+    if number == 1:
         still_calculating = False
 
-print(f"This are the prime factors of {n}: {prime_factors_of_n}")
-print(f"This is the biggest one: {prime_factors_of_n[-1]}")
+print(f"This are the prime factors of {number}: {prime_factors}")
+print(f"This is the biggest one: {prime_factors}")
