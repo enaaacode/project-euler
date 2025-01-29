5f9995143c74f530a8f0fdaa77d9368e3fbe6802
enaaacode <152639159+enaaacode@users.noreply.github.com>
2025-01-29 20:48:04 +0100

    Create file

---
+++ 24_Lexicographic_Permutations.py
@@ -0,0 +1,10 @@
+# Problem 24: Lexicographic Permutations
+n = str(list(range(10)))
+print(n)
+permutations = [] 
+
+for i in range(len(n)):
+    permutation = i
+    for j in range(len(n)):
+        
+        pass
